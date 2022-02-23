36746d954d3e3097aefe75b92a979ec909e9b28c
woo_chaos <785808933@qq.com>
2022-02-24 00:48:25 +0800

    [2022/02/24] 1.修改 简单优化TodoHomeFloatingActionBtn、WidgetFilteredTabBar UI代码嵌套过深的代码结构

--- lib/ichaos/todo/todo-domain/core/widgets/filtered/filtered_tab_bar.dart
+++ lib/ichaos/todo/todo-domain/core/widgets/filtered/filtered_tab_bar.dart
@@ -1,6 +1,4 @@
-
 import 'package:badges/badges.dart';
-import 'package:flustars/flustars.dart';
 import 'package:flutter/material.dart';
 import 'package:i_chaos/base_framework/utils/image_helper.dart';
 import 'package:i_chaos/base_framework/widget_state/widget_state.dart';
@@ -11,6 +9,7 @@ import 'package:i_chaos/ichaos/todo/todo-domain/core/widgets/fba/home_fab_vm.dar
 import 'package:i_chaos/ichaos/todo/todo-domain/core/widgets/todolist/single_todo_list.dart';
 import 'package:provider/provider.dart';
 import 'package:tab_indicator_styler/tab_indicator_styler.dart';
+import 'package:widget_chain/widget_chain.dart';
 
 import 'filtered_tab_bar_vm.dart';
 
@@ -29,87 +28,88 @@ class WidgetFilteredTabBar extends WidgetState {
       TodoState.all: S.of(context).todo_filtered_tab_bar_state_all
     };
 
+    final List<Widget> tabs = [
+      Tab(
+        child: Selector<FilteredTabBarVM, DateTime>(
+            selector: (ctx, vm) => vm.currentDate,
+            shouldRebuild: (pre, next) => true,
+            builder: (ctx, vm, child) {
+              if (_filteredTabBarVM.activeTodoCnt > 0) {
+                return _activeTodoBadgeBuilder(_tabWords);
+              } else {
+                return Text(_tabWords[TodoState.active]!);
+              }
+            }),
+      ),
+      Tab(
+        text: _tabWords[TodoState.completed]!,
+      ),
+    ];
+
     return DefaultTabController(
         length: 2,
         initialIndex: _filteredTabBarVM.currentTabBarIndex,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
+            TabBar(
+              indicatorColor: Colors.teal,
+              isScrollable: false,
+              tabs: tabs,
+              labelColor: Colors.black,
+              indicator: MaterialIndicator(
+                height: 3,
+                topLeftRadius: 8,
+                topRightRadius: 8,
+                horizontalPadding: 80,
+                tabPosition: TabPosition.bottom,
+              ),
+              onTap: (index) async {
+                _filteredTabBarVM.tabIndexSwitch(index);
+              },
+            ).intoContainer(
               height: 45,
-              width: ScreenUtil.getInstance().screenWidth,
+              width: screenWidth,
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(15)),
               ),
-              child: TabBar(
-                  indicatorColor: Colors.teal,
-                  isScrollable: false,
-                  tabs: [
-                    Tab(
-                      child: Selector<FilteredTabBarVM, DateTime>(
-                        selector: (ctx, vm) => vm.currentDate,
-                        shouldRebuild: (pre, next) => true,
-                        builder: (ctx, vm, child) {
-                          return _filteredTabBarVM.activeTodoCnt > 0
-                              ? Badge(
-                                  toAnimate: false,
-                                  badgeContent: Text('${_filteredTabBarVM.activeTodoCnt}'),
-                                  position: BadgePosition(top: _filteredTabBarVM.activeTodoCnt >= 10 ? -1 : -4, end: -30),
-                                  padding: EdgeInsets.all(_filteredTabBarVM.activeTodoCnt >= 10 ? 3 : 6),
-                                  badgeColor: Colors.red,
-                                  child: Text(_tabWords[TodoState.active]!),
-                                )
-                              : Text(_tabWords[TodoState.active]!);
-                        },
-                      ),
-                    ),
-                    Tab(
-                      text: _tabWords[TodoState.completed]!,
-                    ),
-                  ],
-                  labelColor: Colors.black,
-                  indicator: MaterialIndicator(
-                    height: 3,
-                    topLeftRadius: 8,
-                    topRightRadius: 8,
-                    horizontalPadding: 80,
-                    tabPosition: TabPosition.bottom,
-                  ),
-                  onTap: (index) async {
-                    _filteredTabBarVM.tabIndexSwitch(index);
-                  }),
             ),
-            Container(
-                width: ScreenUtil.getScreenW(context),
-                // 减去appBar+日历条+过滤栏(含margin)+bottomBar(含margin)
-                height: ScreenUtil.getInstance().screenHeight - (40 + 68 + 10 + 130),
-                color: Colors.white,
-                child: Selector<FilteredTabBarVM, DateTime>(
-                  selector: (ctx, vm) => vm.currentDate,
-                  shouldRebuild: (pre, next) => true,
-                  builder: (ctx, currentDate, child) {
-                    return TabBarView(
-                      children: getTodoListOrPlaceholder(context, _filteredTabBarVM.busy),
-                    );
-                  },
-                )),
+            Selector<FilteredTabBarVM, DateTime>(
+              selector: (ctx, vm) => vm.currentDate,
+              shouldRebuild: (pre, next) => true,
+              builder: (ctx, currentDate, child) {
+                return TabBarView(
+                  children: getTodoListOrPlaceholder(context, _filteredTabBarVM.busy),
+                );
+              },
+            ).intoContainer(
+              width: screenWidth,
+              // 减去appBar+日历条+过滤栏(含margin)+bottomBar(含margin)
+              height: screenHeight - (40 + 68 + 10 + 130),
+              color: Colors.white,
+            ),
           ],
         ));
   }
 
+  Badge _activeTodoBadgeBuilder(Map<TodoState, String> _tabWords) {
+    return Badge(
+      toAnimate: false,
+      badgeContent: Text('${_filteredTabBarVM.activeTodoCnt}'),
+      position: BadgePosition(top: _filteredTabBarVM.activeTodoCnt >= 10 ? -1 : -4, end: -30),
+      padding: EdgeInsets.all(_filteredTabBarVM.activeTodoCnt >= 10 ? 3 : 6),
+      badgeColor: Colors.red,
+      child: Text(_tabWords[TodoState.active]!),
+    );
+  }
+
   List<Widget> getTodoListOrPlaceholder(BuildContext ctx, bool isBusy) {
     if (isBusy) {
-      Widget loadingImageWidget = Padding(
-        padding: const EdgeInsets.symmetric(vertical: 40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ImageHelper.placeHolderLocalSVGImg(imageName: 'image_loading', width: 100, height: 100),
-            DotLoading(text: S.of(context).todo_filtered_tab_bar_loading_label,)
-          ],
-        ),
-      );
+      Widget loadingImageWidget = ImageHelper.placeHolderLocalSVGImg(imageName: 'image_loading', width: 100, height: 100)
+          .addNeighbor(DotLoading(text: S.of(context).todo_filtered_tab_bar_loading_label))
+          .intoColumn(mainAxisAlignment: MainAxisAlignment.start)
+          .intoPadding(padding: const EdgeInsets.symmetric(vertical: 40));
 
       return [
         loadingImageWidget,
@@ -119,8 +119,14 @@ class WidgetFilteredTabBar extends WidgetState {
       final btnVM = Provider.of<TodoHomeFloatingActionBtnVM>(ctx, listen: false);
 
       return [
-        generateWidget(() => WidgetSingleTodoList(isActive: true, todoListScrollCallback: _filteredTabBarVM.getTodoListNotifyCallback(btnVM, true))),
-        generateWidget(() => WidgetSingleTodoList(isActive: false, todoListScrollCallback: _filteredTabBarVM.getTodoListNotifyCallback(btnVM, false)))
+        generateWidget(() => WidgetSingleTodoList(
+              isActive: true,
+              todoListScrollCallback: _filteredTabBarVM.getTodoListNotifyCallback(btnVM, true),
+            )),
+        generateWidget(() => WidgetSingleTodoList(
+              isActive: false,
+              todoListScrollCallback: _filteredTabBarVM.getTodoListNotifyCallback(btnVM, false),
+            ))
       ];
     }
   }
