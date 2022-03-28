61648310e8a53aee17ccc9dbdf90d9c60556fc94
woo_chaos <785808933@qq.com>
2022-03-29 00:25:00 +0800

    refactor(*): 基类*Controller改为*Ctrl
    1.更新WidgetControllerView的指定类型controller

--- lib/base-getX-framework/view/widget/widget_controller_view.dart
+++ lib/base-getX-framework/view/widget/widget_controller_view.dart
@@ -2,10 +2,10 @@
 
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:i_chaos/base-getX-framework/view-model/base_view_state_ctrl.dart';
+import 'package:i_chaos/base-getX-framework/view-model/widget_view_ctrl.dart';
 import 'package:i_chaos/base-getX-framework/view/base_controller_view.dart';
 
-abstract class WidgetControllerView<T extends BaseViewStateCtrl> extends BaseControllerView<T> {
+abstract class WidgetControllerView<T extends WidgetViewCtrl> extends BaseControllerView<T> {
   WidgetControllerView({Key? key}) : super(key: key);
 
   // 组件刷新
